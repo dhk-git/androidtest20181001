1f20c1721719333f3fbe07026caa721eefbb7aac
dhk <unmix@nate.com>
2018-10-03 21:46:32 +0900

    커스텀 리스트뷰

--- app/src/main/java/com/example/dhkd/testproject/MainActivity.java
+++ app/src/main/java/com/example/dhkd/testproject/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.dhkd.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] items = {"망고", "토마토","포도","딸기","수박","키위","사과","복숭아"};
+        ListAdapter adapter = new ImageAdapter(this,items);
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = String.valueOf(parent.getItemAtPosition(position));
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
